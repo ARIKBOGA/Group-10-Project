78d6f3e3d3e3e8996525180ae1b470bb353b9b15
SerpilOzulu <serpilozturk000@gmail.com>
2022-04-21 23:16:48 +0300

    third commit of Serpil Branch

--- src/Serpil.java
+++ src/Serpil.java
@@ -2,5 +2,6 @@ public class Serpil {
     public static void main(String[] args) {
         System.out.println("hello everyone");
         System.out.println("we are studying on a new project");
+        //this is the third commit
     }
 }
