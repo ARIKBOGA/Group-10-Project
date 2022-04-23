1c66c58c9521d49955441f2072b38b01c61070d8
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-04-23 21:30:53 +0300

    commander burak you should have created a new repo as Oscar

--- src/Yusuf.java
+++ src/Yusuf.java
@@ -2,5 +2,5 @@ public class Yusuf {
     //step1
     //step 2 from yusuf
     // ı dont know what the hell we are doing
-    // commender burak you should have created a new repo as Oscar
+    // commander burak you should have created a new repo as Oscar.
 }
